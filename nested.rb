7894013520c7d0af27db68f91cd086229f1fe4c9
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-11 18:39:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,8 +62,7 @@ end
 def adding_matz
 # add the following information to the top level of programmer_hash
 
-	programmer_hash = 
- 		
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -77,7 +76,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
+      programmer_hash[:yukihiro_matsumoto] = {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+    }
+    programmer_hash
     
 end
 
