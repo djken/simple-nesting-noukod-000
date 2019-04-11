3a34d712fd6fa396c4c6123e1a35f27a89c4a444
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-11 18:39:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,10 +78,7 @@ def adding_matz
         }
      }
      
-     programmer_hash[:yukihiro_matsumoto] = {
-       :known_for => "Ruby",
-       :languages => ["LISP", "C"]
-    }
+    
 end
 
 def changing_alan
